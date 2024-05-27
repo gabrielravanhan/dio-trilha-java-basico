238864fa61dfe29563c19c5b4b022b54352555be
Gabriel Ravanhan <gabriel.ravanhan@outlook.com>
2024-05-26 21:10:25 -0300

    Finalização

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("R$ ###,###,##0.00");
+
+        int numero;
+        String agencia, nomeCliente;
+        double saldo;
+
+        System.out.print("Informe o número da conta: ");
+        numero = sc.nextInt();
+
+        System.out.print("Informe a agência da conta: ");
+        agencia = sc.next();
+
+        System.out.print("Informe o nome do cliente: ");
+        nomeCliente = sc.next();
+
+        System.out.print("Informe o saldo da conta: ");
+        saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo de " + df.format(saldo) + " já está disponível para saque.");
     }
 }
